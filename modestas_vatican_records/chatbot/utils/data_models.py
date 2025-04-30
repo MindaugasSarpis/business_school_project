65ccb7279cb130ffcecc710c7a6b8ee10829a57b
Your Name <you@example.com>
2025-04-30 17:49:33 +0300

    Remove deleted files

--- modestas_vatican_records/chatbot/utils/data_models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class Article(BaseModel):
-    """PDF articles"""
-
-    title: str
-    body: str
--- plotting_sandbox/plotting.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plot_data(df, key, bins=20):
-    df[key].plot(kind='hist', bins=bins)
-    plt.title('Distribution')
-    plt.xlabel(key)
-    plt.savefig(f'plot.png')
-    plt.show()
